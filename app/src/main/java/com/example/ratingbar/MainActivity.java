af2fcbde2e207d921bb54fbfef2ca3195edaecb3
sunil <sunilkhedkar22215@gmail.com>
2020-11-26 10:43:28 +0530

    formatted

--- app/src/main/java/com/example/ratingbar/MainActivity.java
+++ app/src/main/java/com/example/ratingbar/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.ratingbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private RatingBar ratingBar;
@@ -18,15 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         addListenerOnButtonClick();
     }
-    public void addListenerOnButtonClick(){
-        ratingBar=(RatingBar) findViewById(R.id.ratingBar);
-        button=(Button) findViewById(R.id.button);
+
+    public void addListenerOnButtonClick() {
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        button = (Button) findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String rating=String.valueOf(ratingBar.getRating());
-                Toast.makeText(getApplicationContext(),rating,Toast.LENGTH_SHORT).show();
+                String rating = String.valueOf(ratingBar.getRating());
+                Toast.makeText(getApplicationContext(), rating, Toast.LENGTH_SHORT).show();
             }
         });
     }
